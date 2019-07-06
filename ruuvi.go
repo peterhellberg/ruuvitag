c763e30e39319ee3e44d4a0759cd5b6d124bbe4f
Peter Hellberg <peter@c7.se>
2019-07-06 22:04:16 +0200

    Initial version

---
+++ ruuvi.go
@@ -0,0 +1,125 @@
+package ruuvi
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// ManufacturerID sent by RuuviTags
+var ManufacturerID = []byte{0x99, 0x4}
+
+// RAWv1 Data Format 3
+type RAWv1 struct {
+	DataFormat   uint8   // 3
+	Humidity     float64 // 0.0 % to 100 % in 0.5 % increments.
+	Temperature  float64 // -127.99 °C to +127.99 °C in 0.01 °C increments.
+	Pressure     uint32  // 50000 Pa to 115536 Pa in 1 Pa increments.
+	Battery      uint16  // 0 mV to 65536 mV in 1 mV increments, practically 1800 ... 3600 mV.
+	Acceleration         // -32000 to 32000 (mG), however the sensor on RuuviTag supports only 16 G max
+}
+
+// Acceleration vector
+type Acceleration struct {
+	X int16
+	Y int16
+	Z int16
+}
+
+// DecodeRAWv1 from "Manufacturer Specific Data"-field.
+// Manufacturer ID is 0x0499. The actual data payload is:
+//
+// 		Offset 	Allowed values 	Description
+// 		0 				3 							Data format definition (3 = current sensor readings)
+// 		1 				0…200 					Humidity (one lsb is 0.5%, e.g. 128 is 64%)
+// 		2 				-127…127 				Temperature (MSB is sign, next 7 bits are decimal value)
+// 		3 				0…99 						Temperature (fraction, 1/100.)
+// 		4 - 5 		0…65535 				Pressure (Most Significant Byte first, value - 50kPa)
+// 		6-7 			-32767…32767 		Acceleration-X (Most Significant Byte first)
+// 		8 - 9 		-32767…32767 		Acceleration-Y (Most Significant Byte first)
+// 		10 - 11 	-32767…32767 		Acceleration-Z (Most Significant Byte first)
+// 		12 - 13 	0…65535 				Battery voltage (millivolts). MSB First
+//
+// 	https://github.com/ruuvi/ruuvi-sensor-protocols#data-format-3-protocol-specification-rawv1
+//
+func DecodeRAWv1(data []byte) (RAWv1, error) {
+	// Verify the length of the manufacturer data
+	if len(data) != 16 {
+		return RAWv1{}, ErrDataLength
+	}
+
+	// Verify expected Manufacturer ID
+	if !bytes.HasPrefix(data, ManufacturerID) {
+		return RAWv1{}, ErrManufacturerID
+	}
+
+	// Payload in the manufacturer data
+	payload := data[2:16]
+
+	return RAWv1{
+		DataFormat:   payload[0],
+		Humidity:     calculateHumidity(payload[1]),
+		Temperature:  calculateTemperature(payload[2], payload[3]),
+		Pressure:     calculatePressure(payload[4:6]),
+		Acceleration: calculateAcceleration(payload[6:12]),
+		Battery:      calculateBattery(payload[12:14]),
+	}, nil
+}
+
+func calculateHumidity(h byte) float64 {
+	return float64(h) * 0.5
+}
+
+func calculateTemperature(d, f uint8) float64 {
+	t := float64(d&127) + float64(f)*0.01
+
+	if d&128 == 0 {
+		return t
+	}
+
+	return -t
+}
+
+func calculatePressure(b []byte) uint32 {
+	if b == nil {
+		return 0
+	}
+
+	return 50000 + uint32(binary.BigEndian.Uint16(b))
+}
+
+func calculateAcceleration(b []byte) Acceleration {
+	if len(b) != 6 {
+		return Acceleration{}
+	}
+
+	return Acceleration{
+		X: calculateAxis(b[0:2]),
+		Y: calculateAxis(b[2:4]),
+		Z: calculateAxis(b[4:6]),
+	}
+}
+
+func calculateAxis(b []byte) int16 {
+	if b == nil {
+		return 0
+	}
+
+	return int16(binary.BigEndian.Uint16(b))
+}
+
+func calculateBattery(b []byte) uint16 {
+	return binary.BigEndian.Uint16(b)
+}
+
+// Error type
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+// Error constants
+const (
+	ErrDataLength     = Error("unexpected Manufacturer data length")
+	ErrManufacturerID = Error("unknown Manufacturer ID")
+)
---
+++ ruuvi_test.go
@@ -0,0 +1,189 @@
+package ruuvi
+
+import "testing"
+
+func TestDecodeRAWv1(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		for _, tt := range []struct {
+			data []byte
+			want RAWv1
+		}{
+			{
+				[]byte{
+					0x99, 0x04, 0x03, 0x4d,
+					0x17, 0x01, 0xc1, 0x87,
+					0x00, 0x08, 0xff, 0xd5,
+					0x04, 0x1a, 0x0c, 0x1f,
+				},
+				RAWv1{
+					DataFormat:  3,
+					Humidity:    38.5,
+					Temperature: 23.01,
+					Pressure:    99543,
+					Battery:     3103,
+					Acceleration: Acceleration{
+						X: 8,
+						Y: -43,
+						Z: 1050,
+					},
+				},
+			},
+			{
+				[]byte{
+					0x99, 0x04, 0x03, 0x60,
+					0x0f, 0x06, 0xc1, 0xc9,
+					0xff, 0xf4, 0xff, 0xd0,
+					0x04, 0x1c, 0x0c, 0x13,
+				},
+				RAWv1{
+					DataFormat:  3,
+					Humidity:    48,
+					Temperature: 15.06,
+					Pressure:    99609,
+					Battery:     3091,
+					Acceleration: Acceleration{
+						X: -12,
+						Y: -48,
+						Z: 1052,
+					},
+				},
+			},
+			{
+				[]byte{
+					0x99, 0x04, 0x03, 0x49,
+					0x82, 0x01, 0xc1, 0x82,
+					0xff, 0xf9, 0xff, 0xd4,
+					0x04, 0x24, 0x0c, 0x13,
+				},
+				RAWv1{
+					DataFormat:  3,
+					Humidity:    36.5,
+					Temperature: -2.01,
+					Pressure:    99538,
+					Battery:     3091,
+					Acceleration: Acceleration{
+						X: -7,
+						Y: -44,
+						Z: 1060,
+					},
+				},
+			},
+		} {
+			raw, err := DecodeRAWv1(tt.data)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if got, want := raw, tt.want; got != want {
+				t.Fatalf("raw = %+v, want %+v", got, want)
+			}
+		}
+	})
+
+	t.Run("invalid input", func(t *testing.T) {
+		t.Run("unexpected Manufacturer data length", func(t *testing.T) {
+			data := []byte{}
+
+			if _, err := DecodeRAWv1(data); err != ErrDataLength {
+				t.Fatalf("expected error ErrDataLength")
+			}
+		})
+
+		t.Run("unknown Manufacturer ID", func(t *testing.T) {
+			data := []byte{
+				0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0,
+			}
+
+			if _, err := DecodeRAWv1(data); err != ErrManufacturerID {
+				t.Fatalf("expected ManufacturerID")
+			}
+		})
+	})
+}
+
+func TestCalculateHumidity(t *testing.T) {
+	for _, tt := range []struct {
+		h    uint8
+		want float64
+	}{
+		{0x00, 0},
+		{0x80, 64},
+		{0xC8, 100},
+	} {
+		if got := calculateHumidity(tt.h); got != tt.want {
+			t.Fatalf("calculateHumidity(0x%X) = %f, want %f", tt.h, got, tt.want)
+		}
+	}
+}
+
+func TestCalculateTemperature(t *testing.T) {
+	for _, tt := range []struct {
+		d    uint8
+		f    uint8
+		want float64
+	}{
+		{0x00, 0x00, 0.0},
+		{0x81, 0x45, -1.69},
+		{0x01, 0x45, 1.69},
+	} {
+		if got := calculateTemperature(tt.d, tt.f); got != tt.want {
+			t.Fatalf("calculateTemperature(0x%X, 0x%X) = %f, want %f", tt.d, tt.f, got, tt.want)
+		}
+	}
+}
+
+func TestCalculatePressure(t *testing.T) {
+	for _, tt := range []struct {
+		b    []byte
+		want uint32
+	}{
+		{nil, 0},
+		{[]byte{0x00, 0x00}, 50000},
+		{[]byte{0xC8, 0x7D}, 101325}, // (average sea-level pressure)
+		{[]byte{0xFF, 0xFF}, 115535},
+	} {
+		if got := calculatePressure(tt.b); got != tt.want {
+			t.Fatalf("calculatePressure(%X) = %d, want %d", tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestCalculateAcceleration(t *testing.T) {
+	for _, tt := range []struct {
+		b    []byte
+		want Acceleration
+	}{
+		{nil, Acceleration{}},
+		{[]byte{0xFC, 0x18, 0x03, 0xE8, 0x0, 0x0}, Acceleration{-1000, 1000, 0}},
+	} {
+		if got := calculateAcceleration(tt.b); got != tt.want {
+			t.Fatalf("calculateAcceleration(%#v) = %+v, want %+v", tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestCaluclateAxis(t *testing.T) {
+	for _, tt := range []struct {
+		b    []byte
+		want int16
+	}{
+		{nil, 0},
+		{[]byte{0xFC, 0x18}, -1000},
+		{[]byte{0x03, 0xE8}, 1000},
+	} {
+		if got := calculateAxis(tt.b); got != tt.want {
+			t.Fatalf("calculateAxis(%#v) = %d, want %d", tt.b, got, tt.want)
+		}
+	}
+}
+
+func TestError(t *testing.T) {
+	var err error = Error("test error")
+
+	if got, want := err.Error(), "test error"; got != want {
+		t.Fatalf("err.Error() = %q, want %q", got, want)
+	}
+}
